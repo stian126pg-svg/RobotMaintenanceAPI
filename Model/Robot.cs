bb54b3c3b94a0f444986965d2c812d2250f1166c
stian126jg-svg <stian1.26jg@gmail.com>
2026-08-17 13:01:52 +0200

    Added some metadata properties to Robot.cs that works towards validation.

--- Model/Robot.cs
+++ Model/Robot.cs
@@ -1,12 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace RobotMaintenanceApi.Model;
 
 public class Robot
 {
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
     public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(100)]
     public string Model { get; set; } = string.Empty;
+
+    [Required]
     public string Status { get; set; } = string.Empty;
 
     public DateTime LastMaintenance { get; set; }
+
     public DateTime? NextMaintenance { get; set; }
 }
