8e3ee962c5408afc6d92d6db34778b07d35c59d5
stian126jg-svg <stian1.26jg@gmail.com>
2026-08-19 09:53:33 +0200

    Working on creating EF Core support, along with some slight tweaks to overall structure and architecture here.

---
+++ Data/RobotDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using RobotMaintenanceApi.Model;
+
+namespace RobotMaintenanceApi.Data;
+
+public class RobotDbContext : DbContext
+{
+    public RobotDbContext(DbContextOptions<RobotDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Robot> Robots => Set<Robot>();
+}
